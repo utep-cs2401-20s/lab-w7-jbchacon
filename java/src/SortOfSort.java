7bdcf915c9871c222580d60de9f66bc2b7716bea
Julissa Chacon <jbchacon@miners.utep.edu>
2020-03-04 13:47:17 -0800

    in class

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,9 +1,26 @@
-public class SortOfSort {
-    public static void SortOfSort(int num []){
-        for (int i = 0; i<num.length ;i++){
-        if((num.length)/2){
+import java.util.Arrays;
 
-        }
-    }
-    }
-}
+
+public class SortOfSort {
+   public static void SortOfSort(int num []){
+
+
+        int Counter = 0;
+        int pointer1 = 0;
+        int pointer2 = num.length - 1;
+        int max = 0;
+        int temp;
+        int index = 0;
+
+
+        while (pointer1 < pointer2) {
+            max = num[pointer1];
+            for (int i = pointer1 + 1; i <= pointer2; i++) {
+                if (num[i] > max) {
+                    max = num[i];
+                    index = i;
+                }
+            }
+            if (Counter > 4) {
+                Counter = 0;
+            
